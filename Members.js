5ff7c5766afd526e43c2e331a3a95bb19cdcdd43
mazzac409 <mazzachris.work@gmail.com>
2019-06-21 06:51:06 -0500

    initial commit of project work

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'Jane Doe',
+        email: 'jane@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Bob Williams',
+        email: 'bob@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Chris Mazza',
+        email: 'chris@gmail.com',
+        status: 'active'
+    }
+]
+
+module.exports = members;
---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const logger = require('./middleware/logger');
+const members = require('./Members');
+
+const app = express();
+// Init Middleware
+//app.use(logger);
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//Homnepage Route
+app.get('/', (req, res) => res.render('index', {
+    title: 'Members App',
+    members
+}));
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// app.get('/',(req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Members API Routes
+app.use('/api/members', require('./routes/api/members'))
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
